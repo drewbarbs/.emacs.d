4f63782c3c1a62dfaa53dac7df4bbb94ddf7e04a
Andrew Barbarello <andrew.barbarello@outlook.com>
2016-11-20 13:03:29 -0500

    Move (package-initialize) out of emacs-init.org to the top of init.el

--- init.el
+++ init.el
@@ -1,3 +1,23 @@
+;; First, setup load path and package sources by initializing
+;; package.el. This has the benefit of ensuring the latest org-mode is
+;; used to load the emacs-init.org script
+(require 'package)
+(setq my/package-archives '(("melpa" . "https://melpa.org/packages/")
+                            ("org" . "http://orgmode.org/elpa/")))
+(mapc (lambda (archive)
+        (let ((id (car archive))
+              (location (cdr archive)))
+          (unless (assoc-default id package-archives)
+            (add-to-list 'package-archives archive t)))) my/package-archives)
+;; This modifies the load path
+(package-initialize)
+
+;; We are manually calling package-initialize, so we set
+;; package-enable-at-startup to nil to disable the default behavior
+;; of package-initialize being run (again) after processing of the init.el
+;; file (see http://emacs.stackexchange.com/a/5888).
+(setq package-enable-at-startup nil)
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
